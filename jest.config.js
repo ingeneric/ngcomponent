3bbd10c0f4f207e23d2aacfaf22ffea13456d0da
Anton Mokrushin <anton@mokr.org>
2023-07-11 00:01:24 +0400

    jest config refactoring

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,11 @@
 export default {
-    preset: 'ts-jest/presets/default-esm',
-    testEnvironment: 'jsdom',
-    transform: {
-        '^.+\\.ts$': [
-            'ts-jest',
-            {
-                isolatedModules: true,
-            },
-        ],
-    },
-    moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': '$1',
-    },
-    roots: ['<rootDir>/src/'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(t|j)sx?$': ['ts-jest', { useESM: true }],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.(t|j)sx?$': '$1',
+  },
+  roots: ['<rootDir>/src/'],
 };
